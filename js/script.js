36271d65c226c8fdf04c971795097a3221730fc4
Polina Kazakova <websitework521@gmail.com>
2025-04-10 17:53:39 +0200

    more questions

--- js/script.js
+++ js/script.js
@@ -15,5 +15,7 @@ personalMovieDB.count = numberOfFilms;
 const userLastMovie = prompt("Один из последних просмотренных фильмов?", "Mind Hunter");
 personalMovieDB.movies[userLastMovie] = prompt(`На сколько оцениваете фильм '${userLastMovie} по 10 балльной шкале?'`);
 
+const userLastMovie1 = prompt("Один из последних просмотренных фильмов?", "Mind Hunter");
+personalMovieDB.movies[userLastMovie1] = prompt(`На сколько оцениваете фильм '${userLastMovie1} по 10 балльной шкале?'`);
 
 console.log(personalMovieDB);
